04495e7ecd7b30ffcbfa26a75f7e3300f37521ee
willis3333 <60554260+willis3333@users.noreply.github.com>
2022-04-28 15:28:15 -0400

    Update snake_module.py
    Added descriptions

--- snake_module.py
+++ snake_module.py
@@ -1,8 +1,15 @@
 import turtle
 import random
 
+'''
+Snake Module is created to create object oriented code for the snakegame.
+'''
 
 class Snake:
+    '''
+    The Snake class is the default snake object in the game, and can be used to alter the snake's features.
+    It also includes the functions required to create, move, turn, and add length to the snake.
+    '''
     def __init__(self):
         # self.snake.speed('fastest')
         self.segments = []
@@ -12,6 +19,10 @@ class Snake:
         self.snake_length = 3
 
     def snake_create(self):
+        '''
+        Creates three square turtle objects and appends them to segments list and moves them to
+        starting position locations to form a snake
+        '''
         for _ in range(0, self.snake_length):
             coordinates = (self.x, self.y)
             self.positions.append(coordinates)
@@ -25,41 +36,64 @@ class Snake:
             print(self.segments)
 
     def add_segment(self):
+        '''
+        Appends one square turtle object to segments list to add additional length to snake
+        '''
         snake_segment = turtle.Turtle(shape='square')
         snake_segment.penup()
         self.segments.append(snake_segment)
         print(len(self.segments))
 
     def move(self):
-        # TODO: debug snake movement
+        # TODO: debug snake movement on restart of game
+        '''
+        Moves snake segments forward in animated motion with one segment following the next in position
+        '''
         for i in range(len(self.segments)-1, 0, -1):
             self.segments[i].goto(self.segments[i-1].position())
             # print(i, self.segments[i].position())
-            print(self.segments[0].heading())
         self.segments[0].forward(20)
 
     def go_north(self):
+        '''
+        Sets heading of snakes head (1st segment) northbound or 90 degrees only if its not currently southbound
+        '''
         if self.segments[0].heading() != 270:
             self.segments[0].setheading(90)
 
     def go_south(self):
+        '''
+        Sets heading of snakes head (1st segment) southbound or 270 degrees only if its not currently northbound
+        '''
         if self.segments[0].heading() != 90:
             self.segments[0].setheading(270)
 
     def go_east(self):
+        '''
+        Sets heading of snakes head (1st segment) eastbound or 0 degrees only if its not currently westbound
+        '''
         if self.segments[0].heading() != 180:
             self.segments[0].setheading(0)
 
     def go_west(self):
+        '''
+        Sets heading of snakes head (1st segment) westbound or 180 degrees only if its not currently eastbound
+        '''
         if self.segments[0].heading() != 0:
             self.segments[0].setheading(180)
 
 
 class SnakeFood:
+    '''
+    The SnakeFood class is the default snake object in the game, and can be used to alter the snake's features.
+    '''
     def __init__(self):
         self.food = turtle.Turtle(shape='circle')
         self.food.shapesize(1, 1)
         self.food.penup()
 
     def place_food(self):
+        '''
+        places food in random position on the screen
+        '''
         self.food.setposition(random.randint(-200, 200), random.randint(-200, 200))
