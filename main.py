84bd4c91757f768f61e9b43026aab91a7984b501
willis3333 <60554260+willis3333@users.noreply.github.com>
2022-04-28 14:56:46 -0400

    Update gitignore, added TODOs to main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Turtle APPS - powered by Python's turtle library
 
-# Create code which allows user to choose between different programs and then runs depending on choice
+# TODO Create code which allows user to choose between different programs and then runs depending on choice
 
 # click on screen to exit when done
